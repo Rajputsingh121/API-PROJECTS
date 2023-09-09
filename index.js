f6baac3beee667e88e914918d4db996fe1f675c5
Abhijeet <singhrajput8101@gmail.com>
2023-09-09 17:13:36 +0530

    Add index.js

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express=require("express");
 const app=express();
 const port=7070;
 const path=require("path");
+const {v4:uuid4}=require("uuid");
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.set(express.static(path.join(__dirname,"public")));
-app.get("/",(req,res)=>{
-    res.send("serving working well");
-});
+let post =
 app.listen(port,()=>{
     console.log("listining to port:7070");
 });
