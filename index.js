423c357d5c9b29d4dc4449b93287e1c1d4b461b1
Abhijeet <singhrajput8101@gmail.com>
2023-09-09 13:04:24 +0530

    we set engine and send request to server

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express=require("express");
 const app=express();
 const port=7070;
 const path=require("path");
+app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.set(express.static(path.join(__dirname,"public")));
+app.get("/",(req,res)=>{
+    res.send("serving working well");
+});
 app.listen(port,()=>{
     console.log("listining to port:7070");
-})
+});
