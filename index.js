c93f6d7414a0cfe2befb6a0a762f1ef105f673c5
Abhijeet <singhrajput8101@gmail.com>
2023-09-23 07:05:03 +0530

    File setting

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
-  
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-  }
 const path=require("path");
+app.set(express.static(path.join(__dirname,"public")));
 const {v4:uuid4}=require("uuid");
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
-app.set(express.static(path.join(__dirname,"public")));
- app.get("/",(req,res)=>{
-res.send("root is working")
- });
+main().then(()=>{
+console.log("connection sucessful");
+})
+.catch((err)=>{
+    console.log(err);
+})
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/Twitter X');
+  
+    // use `await mongoose.connect('mongodb://user
+
+  }
 app.listen(7070,()=>{
     console.log("listining to port:7070");
 
