1efb84c801f768d0bc6cb13fbe38cf8deb36dd7a
Abhijeet <singhrajput8101@gmail.com>
2023-09-23 06:57:23 +0530

    Add a path and file

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express=require("express");
 const app=express();
-const port=7070;
+const mongoose=require("mongoose");
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  }
 const path=require("path");
 const {v4:uuid4}=require("uuid");
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.set(express.static(path.join(__dirname,"public")));
-let post =
-app.listen(port,()=>{
+ app.get("/",(req,res)=>{
+res.send("root is working")
+ });
+app.listen(7070,()=>{
     console.log("listining to port:7070");
+
 });
